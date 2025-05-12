6d8d7c49ae743d25564fd7567a51c0a6ad51b91c
jllm <jllm@jllm-HP-Laptop-15s-fq5xxx>
2025-05-12 08:54:33 +0200

    Versión 1.0.3

--- para_git/src/para_git/menu.java
+++ para_git/src/para_git/menu.java
@@ -4,7 +4,7 @@ public class menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("version 1.0.6");
+		System.out.println("version 1.0.3");
 		System.out.println("version 1.0.2");
 		System.out.println("version 1.0.1");
 
